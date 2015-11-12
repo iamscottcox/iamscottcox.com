b495522205b3b850810edeb32cce79dc932a150b
sulkosaurus <scottcox89@gmail.com>
2015-11-12 22:58:54 +0000

    Added a widget area for the Treehouse widget. Also started constructing the grid area for the Portfolio page.

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 // Enable Theme Support
 add_theme_support( 'menus' );
@@ -7,17 +7,17 @@ add_theme_support( 'post-formats', array( 'image', 'quote' ) );
 add_theme_support( 'html5' );
 
 // Load Theme CSS
-function theme_styles() {	
+function theme_styles() {
 
-	wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/css/bootstrap.css' );	
-	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );	
+	wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/css/bootstrap.css' );
+	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
 
 }
 add_action( 'wp_enqueue_scripts', 'theme_styles' );
 
 // Load Theme JS
 function theme_js() {
-	
+
 	wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/js/bootstrap.js', array('jquery'), '', true );
 	wp_enqueue_script( 'theme_js', get_template_directory_uri() . '/js/theme.js', array('jquery','bootstrap_js'), '', true );
 
@@ -28,7 +28,7 @@ add_action( 'wp_enqueue_scripts', 'theme_js' );
 function register_theme_menus() {
   register_nav_menus(
     array(
-      'header-menu' => __( 'Main Menu' )      
+      'header-menu' => __( 'Main Menu' )
     )
   );
 }
@@ -39,8 +39,8 @@ add_action( 'init', 'register_theme_menus' );
 function create_widget($name, $id, $description) {
 
 	register_sidebar(array(
-		'name' => __( $name ),	 
-		'id' => $id, 
+		'name' => __( $name ),
+		'id' => $id,
 		'description' => __( $description ),
 		'before_widget' => '<div class="widget">',
 		'after_widget' => '</div>',
@@ -50,8 +50,16 @@ function create_widget($name, $id, $description) {
 
 }
 
-// Create widgets 
+// Create widgets
 create_widget("Primary Sidebar", "primary", "Displays on the side of most pages");
 create_widget("Blog Sidebar", "blog", "Displays on the side of the blog section");
+create_widget("Treehouse Sidebar", "treehouse", "Put your Treehouse widget in here");
+
+// the_excerpt();
+
+function custom_excerpt_length( $length ) {
+	return 20;
+}
+add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 
 ?>
--- page-portfolio.php
+++ page-portfolio.php
@@ -1,48 +1,60 @@
 <?php
 /*
-	Template Name: Portfolio Page
+Template Name: Portfolio Page
 */
 
- ?>
+?>
 <?php get_header(); ?>
 
-    <div class="container" role="main">
+<div class="container" role="main">
 
-		<div class="row">
+  <div class="row">
 
-	    	<div class="col-md-8">
+    <div class="col-md-8">
 
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-				    <div class="page-header">
-				    	<h1><?php the_title(); ?></h1>
-				    </div>
+        <div class="page-header">
+          <h1><?php the_title(); ?></h1>
+        </div>
 
-					<?php the_content(); ?>
+        <?php the_content(); ?>
 
-				<?php endwhile; endif; ?>
+      <?php endwhile; endif; ?>
 
-				<?php
-				    $args = array(
-				        'post_type' => 'portfolio'
-				    );
-				    $the_query = new WP_Query( $args );
-				?>
-				<?php if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+      <?php
+      $args = array(
+        'post_type' => 'portfolio'
+      );
+      $the_query = new WP_Query( $args );
+      ?>
+      <?php if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
-				<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+        <div class-"row">
+          <a class="portfolio-link" href="<?php the_permalink(); ?>">
+            <div class="col-xs-6 portfolio-content">
+              <h3 class="text-center"><?php the_title(); ?></h3>
+              <div class="text-center">
+                <?php the_post_thumbnail('thumbnail'); ?>
+              </div>
+              <p class="portfolio-excerpt"><?php the_excerpt(); ?></p>
+            </div>
+          </a>
+        </div>
 
-				<?php endwhile; endif; ?>
+        <!-- <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3> -->
 
-	    	</div>
-
-	    	<?php get_sidebar(); ?>
-
-	    </div>
-
-
-	    </div>
+      <?php endwhile; endif; ?>
 
     </div>
+
+    <?php get_sidebar(); ?>
+
+  </div>
+
+
+</div>
+
+</div>
 
 <?php get_footer(); ?>
