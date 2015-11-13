1eb01d41a165fe390523b40351b153dd09753d83
sulkosaurus <scottcox89@gmail.com>
2015-11-13 00:30:57 +0000

    Added sidebar-treehouse.php to bring in the Treehouse Widget wherever necessary

---
+++ sidebar-treehouse.php
@@ -0,0 +1,6 @@
+<div class="col-md-4">
+	<?php if ( ! dynamic_sidebar( 'treehouse' ) ): ?>
+	<h3>Widget Setup</h3>
+	<p>Please add the Treehouse to have it display here</p>
+	<?php endif; ?>
+</div>
