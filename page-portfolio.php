e0a9284e40adccf778e1c246e6ef291f6128bd73
sulkosaurus <scottcox89@gmail.com>
2015-11-15 10:30:41 +0000

    page-portfolio.php .page-header given .col-sm-12 to give margin-left. Also moved containers around a little to seperate sections of code into chunks.

--- page-portfolio.php
+++ page-portfolio.php
@@ -4,15 +4,15 @@ Template Name: Portfolio Page
 */
 
 ?>
+
 <?php get_header(); ?>
 
 <div class="container" role="main">
-
   <div class="row">
 
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-      <div class="page-header">
+      <div class="page-header col-sm-12">
         <h1><?php the_title(); ?></h1>
       </div>
 
@@ -20,37 +20,34 @@ Template Name: Portfolio Page
 
     <?php endwhile; endif; ?>
 
-    <?php
-    $args = array(
-      'post_type' => 'portfolio'
-    );
-    $the_query = new WP_Query( $args );
-    ?>
-    <?php if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-
-      <div class-"row">
-        <a class="portfolio-link" href="<?php the_permalink(); ?>">
-          <div class="col-sm-4">
-            <div class="content-block">
-              <div class="text-center">
-                <?php the_post_thumbnail('thumbnail'); ?>
-              </div>
-              <h4 class="text-center"><?php the_title(); ?></h4>
-            </div>
-          </a>
-        </div>
-      </div>
-
-
-      <!-- <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3> -->
-
-    <?php endwhile; endif; ?>
-
   </div>
-
-
 </div>
 
+<div class="container">
+  <?php
+  $args = array(
+    'post_type' => 'portfolio'
+  );
+  $the_query = new WP_Query( $args );
+  ?>
+  <?php if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+    <div class-"row">
+      <a class="portfolio-link" href="<?php the_permalink(); ?>">
+        <div class="col-sm-4">
+          <div class="content-block">
+            <div class="text-center">
+              <?php the_post_thumbnail('thumbnail'); ?>
+            </div>
+            <h4 class="text-center"><?php the_title(); ?></h4>
+          </div>
+        </a>
+      </div>
+    </div>
+
+  <?php endwhile; endif; ?>
 </div>
+
+
 
 <?php get_footer(); ?>
