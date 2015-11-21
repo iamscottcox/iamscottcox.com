473c8a41215cb29c5936fbce9cee59ce677ffde8
sulkosaurus <scottcox89@gmail.com>
2015-11-21 11:03:58 +0000

    Changed the order in style.scss so that all styles load correctly.

--- front-page.php
+++ front-page.php
@@ -17,71 +17,79 @@
 </div>
 
 <div class="container">
-  <div class="row">
-    <div class="col-md-6 portfolio">
-      <a href="<?php echo site_url(); ?>/portfolio"><h2 class="text-center home-content-header">Portfolio</h2></a>
-
-      <?php
-      $portfolio_args = array(
-        'post_type' => 'portfolio',
-        'posts_per_page' => 2
-      );
-      $portfolio_query = new WP_Query( $portfolio_args );
-      ?>
-      <?php if ( have_posts() ) : while ( $portfolio_query->have_posts() ) : $portfolio_query->the_post();
-      ?>
-
-      <div class="row front-page-content">
-        <div class="col-sm-4">
-          <a href="<?php the_permalink(); ?>">
-            <div class="text-center post-thumbnail">
-              <?php the_post_thumbnail('thumbnail'); ?>
-            </div>
-          </div>
-          <div class="col-sm-8">
-            <h3><?php the_title(); ?></h3>
-            <p><?php the_excerpt(); ?></p>
-          </div>
-        </a>
+  <a href="<?php echo site_url(); ?>/portfolio">
+    <div class="row portfolio">
+      <div class="col-sm-12">
+        <h2 class="text-center">Portfolio</h2>
       </div>
+    </div>
+  </a>
+</div>
 
-      <hr>
-
-    <?php endwhile; endif; ?>
-
-  </div>
-
-  <div class="col-md-6 blog">
-    <a href="<?php echo site_url(); ?>/blog"><h2 class="text-center home-content-header">Blog</h2></a>
-
+<div class="container-fluid">
+  <div class="row portfolio">
 
     <?php
     $portfolio_args = array(
-      'post_type' => 'post',
-      'posts_per_page' => 2
+      'post_type' => 'portfolio',
+      'posts_per_page' => 3
     );
     $portfolio_query = new WP_Query( $portfolio_args );
     ?>
     <?php if ( have_posts() ) : while ( $portfolio_query->have_posts() ) : $portfolio_query->the_post();
     ?>
 
-    <div class="row front-page-content">
+    <article class="col-sm-4">
       <a href="<?php the_permalink(); ?>">
-        <div class="col-sm-4">
-          <div class="text-center post-thumbnail">
+        <div class="portfolio-tile">
+          <div class="text-center portfolio-thumbnail">
             <?php the_post_thumbnail('thumbnail'); ?>
           </div>
-        </div>
-        <div class="col-sm-8">
-          <h3><?php the_title(); ?></h3>
-          <p><?php the_excerpt(); ?></p>
+          <h3 class="text-center"><?php the_title(); ?></h3>
         </div>
       </a>
-    </div>
+    </article>
 
-    <hr>
+
+
+
 
   <?php endwhile; endif; ?>
+
+</div>
+<hr>
+
+<div class="col-md-6 blog">
+  <a href="<?php echo site_url(); ?>/blog"><h2 class="text-center home-content-header">Blog</h2></a>
+
+
+  <?php
+  $blog_args = array(
+    'post_type' => 'post',
+    'posts_per_page' => 3
+  );
+  $blog_query = new WP_Query( $blog_args );
+  ?>
+  <?php if ( have_posts() ) : while ( $blog_query->have_posts() ) : $blog_query->the_post();
+  ?>
+
+  <div class="row">
+    <a href="<?php the_permalink(); ?>">
+      <div class="col-sm-4">
+        <div class="text-center post-thumbnail">
+          <?php the_post_thumbnail('thumbnail'); ?>
+        </div>
+      </div>
+      <div class="col-sm-8">
+        <h3><?php the_title(); ?></h3>
+        <p><?php the_excerpt(); ?></p>
+      </div>
+    </a>
+  </div>
+
+  <hr>
+
+<?php endwhile; endif; ?>
 
 </div>
 
