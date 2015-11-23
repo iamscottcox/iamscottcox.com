9ea1986af918a40904697ff0c7d060c6cb57cb23
sulkosaurus <scottcox89@gmail.com>
2015-11-23 23:05:09 +0000

    Changed contact details from h3 to p.

--- page-contact.php
+++ page-contact.php
@@ -23,10 +23,10 @@
 
   <div class="row contact-row">
     <div class="col-sm-6 contact-mail text-center">
-      <a href="mailto:scottcox89@gmail.com"><h3><span class="glyphicon glyphicon-envelope"></span>scottcox89@gmail.com</h3></a>
+      <a href="mailto:scottcox89@gmail.com"><p><span class="glyphicon glyphicon-envelope"></span>scottcox89@gmail.com</p></a>
     </div>
     <div class="col-sm-6 contact-phone text-center">
-      <a href="tel:07780604094"><h3><span class="glyphicon glyphicon-earphone"></span>07780604094</h3></a>
+      <a href="tel:07780604094"><p><span class="glyphicon glyphicon-earphone"></span>07780604094</p></a>
     </div>
 
   </div>
