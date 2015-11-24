35740a029ac2c4dc61223a2c02349ea595e7b75a
sulkosaurus <scottcox89@gmail.com>
2015-11-24 23:25:50 +0000

    single and single-portfolio have now been tweaked to match the styling in the rest of the site.

--- single-portfolio.php
+++ single-portfolio.php
@@ -3,21 +3,23 @@
 <div class="container" role="main">
   <div class="row">
     <div class="col-md-8">
-      <div class="page-header">
-        <h3><a href="<?php bloginfo( 'url' ); ?>/portfolio"><span class="glyphicon glyphicon-arrow-left"></span>Back to Portfolio</a></h3>
+
+      <div class="row back-to-blog-row">
+        <div class="col-sm-12">
+          <h3><a href="<?php bloginfo( 'url' ); ?>/blog"><span class="glyphicon glyphicon-arrow-left"></span>Back to Blog</a></h3>
+        </div>
       </div>
+
+      <hr>
 
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-        <div class="row">
-          <div class="col-sm-12">
-
-            <div class="page-header">
-              <h1><?php the_title(); ?></h1>
-              <p class="meta">
-                By <?php the_author_posts_link(); ?> on <?php echo the_time('l, F jS, Y'); ?>
-              </p>
-            </div>
+        <div class="row single-background">
+          <div class="col-md-12">
+            <h1><?php the_title(); ?></h1>
+            <p class="meta">
+              By <?php the_author_posts_link(); ?> on <?php echo the_time('l, F jS, Y'); ?>
+            </p>
 
             <?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'small' );
             $url = $thumb['0']; ?>
@@ -25,23 +27,32 @@
             <div class="single-image" style="background: url(<?php echo $url ?>) no-repeat top center; background-size: cover; background-color: #fff">
             </div>
 
-            <p class="single-content"><?php the_content(); ?></p>
+            <div class="single-content">
+              <?php the_content(); ?>
+            </div>
 
           </div>
         </div>
 
-        <hr>
+      <div class="row">
+        <div class="col-sm-12">
+          <p>Post Type: <span class="post-type"><?php echo get_post_format(); ?></span> |
+            Category: <?php the_category( ', ' );?> |
+            <?php the_tags('Tags: ', ', ');?></p>
+          </div>
+        </div>
 
+
+        <hr>
 
       <?php endwhile; endif; ?>
 
-    </div>
+    </div> <!-- End col-md-8 -->
 
-    <div class="col-md-4">
-      <?php get_sidebar('blog'); ?>
-    </div>
+    <?php get_sidebar( 'blog' ); ?>
 
   </div>
+
 </div>
 
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,12 +1,10 @@
 <?php get_header(); ?>
 
 <div class="container" role="main">
-
   <div class="row">
-
     <div class="col-md-8">
 
-      <div class="row">
+      <div class="row back-to-blog-row">
         <div class="col-sm-12">
           <h3><a href="<?php bloginfo( 'url' ); ?>/blog"><span class="glyphicon glyphicon-arrow-left"></span>Back to Blog</a></h3>
         </div>
@@ -16,18 +14,17 @@
 
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-        <row class="single-header-row">
-          <div class="col-sm-12 page-header">
+        <div class="row single-background">
+          <div class="col-md-12">
             <h1><?php the_title(); ?></h1>
             <p class="meta">
               By <?php the_author_posts_link(); ?> on <?php echo the_time('l, F jS, Y'); ?>
             </p>
-          </div>
-        </row>
 
         <?php if( has_post_format( 'image' )): ?>
           <p><?php the_post_thumbnail('medium'); ?></p>
           <?php the_content(); ?>
+
         <?php elseif ( has_post_format( 'quote' )): ?>
           <blockquote>
             <?php the_excerpt(); ?>
@@ -36,49 +33,39 @@
 
         <?php else: ?>
 
-          <div class="row">
-            <div class="col-sm-12">
-
               <?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'small' );
               $url = $thumb['0']; ?>
 
               <div class="single-image" style="background: url(<?php echo $url ?>) no-repeat top center; background-size: cover; background-color: #fff">
               </div>
 
+              <div class="single-content">
+                <?php the_content(); ?>
+              </div>
+
             </div>
           </div>
-
-          <div class="row">
-            <div class="col-sm-12">
-              <p class="single-content"><?php the_content(); ?></p>
-            </div>
-          </div>
-
-
-
-
 
         <?php endif; ?>
 
+        <div class="row">
+          <div class="col-sm-12">
+            <p>Post Type: <span class="post-type"><?php echo get_post_format(); ?></span> |
+            Category: <?php the_category( ', ' );?> |
+            <?php the_tags('Tags: ', ', ');?></p>
+          </div>
+        </div>
 
-        <hr>
 
-        <!-- <p>
-          Post Type: <span class="post-type"><?php echo get_post_format(); ?></span> |
-          Category: <?php the_category( ', ' );?> |
-          <?php the_tags('Tags: ', ', ');?>
+      <hr>
 
-        </p>
+    <?php endwhile; endif; ?>
 
-        <hr> -->
+    </div> <!-- End col-md-8 -->
 
-      <?php endwhile; endif; ?>
+  <?php get_sidebar( 'blog' ); ?>
 
-    </div>
-
-    <?php get_sidebar( 'blog' ); ?>
-
-  </div>
+</div>
 
 </div>
 
