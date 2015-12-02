5afb358624bf3631ace3bcb66b460f2bf1022001
sulkosaurus <scottcox89@gmail.com>
2015-12-02 15:20:23 +0000

    Added .text-center jumbotron.

--- front-page.php
+++ front-page.php
@@ -3,10 +3,10 @@
 <div class="jumbotron">
   <div class="container">
     <div class="row">
-      <div class="col-md-12">
+      <div class="col-md-12 text-center">
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-          <h1 class="text-center"><?php bloginfo('title'); ?></h1>
+          <h1><?php bloginfo('title'); ?></h1>
 
           <p><?php the_content(); ?></p>
 
